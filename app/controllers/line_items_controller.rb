e4e23edf71aaec42957992398761cad431843564
Javier <65590523+javierc435@users.noreply.github.com>
2020-10-09 15:51:38 +0200

    Updated controllers and cart model

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -58,9 +58,10 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
+    @cart = Card.find(session[:cart_id])
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to cart_path(@cart), notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -73,6 +74,6 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:instrument_id, :cart_id)
+      params.require(:line_item).permit(:instrument_id)
     end
 end
